4be28d2d54ee35d0d2d6b0674501e54e9471749c
Adam Smigacz <adam.smigacz@gmail.com>
2016-08-13 19:40:13 +0200

    Sobota 19 40

---
+++ SQL/cw_14.sql
@@ -0,0 +1,16 @@
+CREATE TABLE clob_content(
+id INTEGER PRIMARY KEY
+, clob_column CLOB NOT NULL);
+/
+
+CREATE TABLE blob_content(
+id INTEGER PRIMARY KEY
+, clob_column BLOB NOT NULL);
+
+/
+CREATE TABLE bfile_content(
+id INTEGER PRIMARY KEY
+, clob_column BFILE NOT NULL);
+/
+
+
