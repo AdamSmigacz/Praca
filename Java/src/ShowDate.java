c1084118cb5ad79e94718f54c7c5ab0f99ab8e3e
Adam Smigacz <adam.smigacz@comarch.com>
2016-11-08 16:22:51 +0100

    dzien 8-11-2016

---
+++ Java/src/ShowDate.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+
+public class ShowDate {
+
+
+	public static void main(String[] args) {
+	
+		System.out.println("Czesc, dzis jest: " + new Date());
+		
+	}
+
+}
---
+++ Java/src/t_3/Operatory.java
@@ -0,0 +1,69 @@
+package t_3;
+
+import java.util.Random;
+
+
+class LevelNumber{
+	int iLevel;
+}
+
+public class Operatory {
+
+	public static void showReferenceDifference(){
+		// przy przypisywaniu obiektu do obiektu kopiowana jest referencja!!! A wiec po operacj przypisania zmianay wprowadzane w jednym obiekcie wprowadzane sa tez do drugiego
+		// przypisywanie pol pozwala zachowac oba obiekty
+		LevelNumber ln1 = new LevelNumber();
+		LevelNumber ln2 = new LevelNumber();
+		
+		ln1.iLevel = 22;
+		ln2.iLevel = 33;
+		
+		System.out.println("Liczba pierwsza to: " + ln1.iLevel + ", a liczba druga to: " + ln2.iLevel);
+		
+		
+		System.out.println(ln1.equals(ln2));
+		
+		ln1.iLevel = ln2.iLevel;
+		ln2.iLevel = 34;
+		
+		System.out.println("Liczba pierwsza to: " + ln1.iLevel + ", a liczba druga to: " + ln2.iLevel);
+		
+		ln1 = ln2;
+
+		System.out.println("Liczba pierwsza to: " + ln1.iLevel + ", a liczba druga to: " + ln2.iLevel);
+		ln1.iLevel = 55;
+		
+		System.out.println("Liczba pierwsza to: " + ln1.iLevel + ", a liczba druga to: " + ln2.iLevel);
+	}
+	
+	public static void RandomNumbers(){
+		// random musi przyjowac liczby > 0
+		Random rand = new Random();
+		for(int i = 0; i < 10; i++)
+		System.out.println("Losowa liczba: " + (rand.nextInt(11)-20));
+	}
+
+	public static void Inkrementacja(){
+		//++a najpierw + 1 a potem zwracany wynik
+		//a++ najpierw zwracany wynik a potem +1
+		int i = 1;
+		System.out.println("Preinkrementacja: " + ++i);
+		System.out.println("Postinkrementacja: " + i++);
+		System.out.println("Argument: " + i);
+		System.out.println("Predekrementacja: " + --i);
+		System.out.println("Postdekrementacja: " + i--);
+		System.out.println("Argument: " + i);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		showReferenceDifference();
+		//Inkrementacja();
+		//RandomNumbers();
+		
+
+
+	}
+
+}
