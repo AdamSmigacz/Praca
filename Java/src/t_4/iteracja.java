74d52b6b47de0a1e77c028101d278bfdeeb5aac8
Adam Smigacz <adam.smigacz@comarch.com>
2016-11-15 08:16:07 +0100

    strona 142 wtorek

--- Java/src/t_4/iteracja.java
+++ Java/src/t_4/iteracja.java
@@ -10,6 +10,23 @@ public class iteracja {
 		return result;
 		
 	}
+	public static void breakContinue()
+	{
+		
+		for (int i = 0; i < 100; i++) {
+			
+			if (i == 30){ 
+				System.out.println("Przerwanie");
+				break;
+			}
+		if (i>10)
+		{
+			System.out.println("Liczba: " + i);
+			continue;
+		}
+		System.out.println("Wykonanie petli!");
+		}
+	}
 	
 	public static void showChar(){
 		for (char i = 0; i < 128; i++) {
@@ -28,7 +45,25 @@ public class iteracja {
 		{
 			System.out.println(x);
 		}
-		
+	}
+	
+	public static void switchFunction(int Operator)
+	{
+		// switch dziaa tylko z selektorami calkowitymi. Nie dziala on z float double itp.
+		switch (Operator) {
+		case 1:
+			System.out.println("Value: " + 1);
+			break;
+		case 2:
+			System.out.println("Value: " + 2);
+			break;
+		case 3:
+			System.out.println("Value: " + 3);
+			break;
+		default:
+			System.out.println("Default value: " + 100);
+			break;
+		}
 	}
 	public static void main(String[] args) {
 /*
@@ -38,7 +73,11 @@ public class iteracja {
 	
 		}*/
 		//showChar();
-		loopForeach();
+		//loopForeach();
+		//breakContinue();
+		//switchFunction(4);
+		
+		// strona 142
 	}
 
 }
